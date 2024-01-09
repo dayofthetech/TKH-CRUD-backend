97f3d879ac1da94d0ba503d2269ddadd70344638
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-01-09 17:14:19 -0500

    CRUD - Create

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+})
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json());
+
+app.post('/users',(req, res) => {
+    console.log(req.body);;
 })
 
 app.listen(PORT, function() {
