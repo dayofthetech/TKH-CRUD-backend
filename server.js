5635f7d5b20264ac90ec0ee45fadcced2ac11f46
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-01-08 21:13:07 -0500

    assigment completed - check README

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Is this thing on?')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(PORT, function() {
+	console.log(`Server is live and update! Listening at port ${PORT}`);
+
+})
+
+
