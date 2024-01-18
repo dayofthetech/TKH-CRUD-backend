a8be47f99164376f85db5d8715706bbf7d73a187
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-01-17 21:17:03 -0500

    CRUD - DELETE

--- server.js
+++ server.js
@@ -9,11 +9,18 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
-MongoClient.connect(process.env.MONGO_URI).then(client => {
-    const db = client.db('practice');
-    const usersCollection = db.collection('users');
 
-    app.get('/', (req, res) => {
+let usersCollection;
+
+MongoClient.connect(process.env.MONGO_URI)
+    .then(client => {
+     const db = client.db('practice');
+     const usersCollection = db.collection('users');
+
+    // leaving '' empty works - but if / no work
+    // if I add /users no work
+    // will remove all /users and leave blank
+    app.get('', (req, res) => {
         usersCollection
             .find()
             .toArray()
@@ -32,17 +39,55 @@ MongoClient.connect(process.env.MONGO_URI).then(client => {
             .catch(error => console.log(error))
         console.log(req.body);
     })
+
+    app.put('/users', (req, res) => {
+        usersCollection
+            .findOneAndUpdate(
+                {username: req.body.username},
+                {
+                    $set: {
+                        username: req.body.username,
+                        password: req.body.password,
+                    },
+                },
+                {
+                    upsert: false,
+                },
+                {
+                    returnNewDocument: true
+                }
+            )
+            .then(result => {
+                res.json('Sucess')
+                return res
+            })
+            .catch(error => console.log(error))
+    })
+
+    app.delete('/users', (req, res) => {
+        console.log('Request Body:', req.body);
+
+        usersCollection
+            .deleteOne(
+                { username: req.body.username }
+            )
+            .then(result => {
+                console.log(`Deleted ${req.body.username}`)
+                console.log(result);
+                res.json('Deleted user')
+            })
+            .catch(error => console.error(error))
+    })
 })
 .catch(error => console.log(error))
 
-
-app.post('/users',(req, res) => {
-    console.log(req.body);
-})
+// leaving this comment out makes the app works ??
+// app.post('/users',(req, res) => {
+//     console.log(req.body);
+// })
 
 app.listen(PORT, function() {
 	console.log(`Server is live and update! Listening at port ${PORT}`);
-
 })
 
 
