88c5b506477c2a89d046d65fd0bdb7c523b8d223
dayofthetech <j.alberto.reyes.r90@gmail.com>
2024-01-26 14:24:36 -0500

    CRUD 7

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
-// const PrismaClient = require('@prisma/client')
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const MongoClient = require('mongodb').MongoClient;
-// const app = express();
-// const PORT = 3000;
-// require('dotenv').config({path: '.env'})
-// app.set('view engine', 'ejs')
-// app.use(express.static('public'))
-// app.use(bodyParser.urlencoded({ extended: true}))
-// app.use(bodyParser.json());
-// let usersCollection;
-// const prisma = new PrismaClient();
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv');
 
@@ -29,10 +15,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const prisma = new PrismaClient();
+app.get('/', async (req, res) => {
+    const body = { users: null, posts: null }
 
+    const users = await prisma.user
+    .findMany()
+        .then(results => {
+            body.users = results;
+    })
+    .catch(error => console.error(error))
 
+    const posts = await prisma.post
+    .findMany()
+        .then(results => {
+            body.posts = results;
+    })
+    .catch(error => console.error(error));
+
+    // storing results in users obj
+    res.render('index.ejs', {body: body})
+})
+// 1. Objective: Create your own POST request for your posts
+// app.post('/posts', async (req, res) => {
+//     const { title, body } = req.body;
+
+//     await prisma.post.create({
+//         data: {
+//             title,
+//             body,
+//             user: {
+//                 create: {
+//                     title,
+//                     body,
+//                     username: String
+//                 }
+//             }
+//         }
+//     }).then(result => {
+//         res.redirect('/');
+//     }).catch(e => {
+//         console.error(e);
+//         res.status(500).json({error: 'Internal Error'})
+//     })
+// })
+
+// This is the first route to post or create users
+// but this is a global app.post
 app.post('/users',(req, res) => {
     const {username, password} = req.body;
+    // this prisma method creates the new user
+    //  and a Post entry with the same title and body
     prisma.user.create({
         data: {
             username,
@@ -55,76 +87,77 @@ app.post('/users',(req, res) => {
     })
 })
 
-MongoClient.connect(process.env.MONGO_URI)
-    .then(client => {
-     const db = client.db('practice');
-     const usersCollection = db.collection('users');
+// MongoClient.connect(process.env.MONGO_URI)
+//     .then(client => {
+//      const db = client.db('practice');
+//      const usersCollection = db.collection('users');
 
     // leaving '' empty works - but if / no work
     // if I add /users no work
     // will remove all /users and leave blank
-    app.get('/', async (req, res) => {
-        const body = { users: null, posts: null }
+    // app.get('/', async (req, res) => {
+    //     const body = { users: null, posts: null }
 
-        const users = await prisma.user
-        .findMany()
-            .then(results => {
-                body.users = results;
-        })
-        .catch(error => console.error(error))
+    //     const users = await prisma.user
+    //     .findMany()
+    //         .then(results => {
+    //             body.users = results;
+    //     })
+    //     .catch(error => console.error(error))
 
-        const posts = await prisma.post
-        .findMany()
-            .then(results => {
-                body.posts = results;
-        })
-        .catch(error => console.error(error));
+    //     const posts = await prisma.post
+    //     .findMany()
+    //         .then(results => {
+    //             body.posts = results;
+    //     })
+    //     .catch(error => console.error(error));
 
-        res.render('index.ejs', {body: body})
-    })
+    //     // storing results in users obj
+    //     res.render('index.ejs', {body: body})
+    // })
 
 
 
-    app.put('/users', (req, res) => {
-        usersCollection
-            .findOneAndUpdate(
-                {username: req.body.username},
-                {
-                    $set: {
-                        username: req.body.username,
-                        password: req.body.password,
-                    },
-                },
-                {
-                    upsert: false,
-                },
-                {
-                    returnNewDocument: true
-                }
-            )
-            .then(result => {
-                res.json('Sucess')
-                return res
-            })
-            .catch(error => console.log(error))
-    })
+//     app.put('/users', (req, res) => {
+//         usersCollection
+//             .findOneAndUpdate(
+//                 {username: req.body.username},
+//                 {
+//                     $set: {
+//                         username: req.body.username,
+//                         password: req.body.password,
+//                     },
+//                 },
+//                 {
+//                     upsert: false,
+//                 },
+//                 {
+//                     returnNewDocument: true
+//                 }
+//             )
+//             .then(result => {
+//                 res.json('Sucess')
+//                 return res
+//             })
+//             .catch(error => console.log(error))
+//     })
 
-    app.delete('/users', (req, res) => {
-        console.log('Request Body:', req.body);
+//     app.delete('/users', (req, res) => {
+//         console.log('Request Body:', req.body);
 
-        usersCollection
-            .deleteOne(
-                { username: req.body.username }
-            )
-            .then(result => {
-                console.log(`Deleted ${req.body.username}`)
-                console.log(result);
-                res.json('Deleted user')
-            })
-            .catch(error => console.error(error))
-    })
-})
-.catch(error => console.log(error))
+//         usersCollection
+//             .deleteOne(
+//                 { username: req.body.username }
+//             )
+//             .then(result => {
+//                 console.log(`Deleted ${req.body.username}`)
+//                 console.log(result);
+//                 res.json('Deleted user')
+//             })
+//             .catch(error => console.error(error))
+//     })
+// })
+// .catch(error => console.log(error))
 
 // leaving this comment out makes the app works ??
 // app.post('/users',(req, res) => {
